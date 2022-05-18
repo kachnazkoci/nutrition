354ac76a8d8607b1e7e15500cba4d4ee6265dec2
Katerina Kratka <katka.kratkaa@gmail,com>
2022-05-18 22:26:00 +0200

    Added templates for food and user. Users templates adjusted according to base simultaneously.

--- user/views.py
+++ user/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, TemplateView, FormView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .forms import UserForm
 from .models import User
+
+
+def user_list_view(request):
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'users.html', context)
+
+
+def user_search_view(request):
+    context = {}
+    return render(request, 'search/search.html',
+                  context=context)
 
 
 class UserListView(ListView):
@@ -8,43 +25,34 @@ class UserListView(ListView):
     template_name = 'users.html'
     extra_context = {'page_name': 'Users'}
 
-    def user_list_view(request):
-        users = User.objects.all()
-        context = {
-            'users': users
-        }
-        return render(request, 'user_list.html', context)
-
-    def user_search_view(request):
-        context = {}
-        return render(request, 'nutrition/user/templates/search/search.html',
-                      context=context)
-
 
 class UserDetailView(DetailView):
     model = User
     template_name = 'user_detail.html'
     extra_context = {'page_name': User}
+
     def get_context_data(self, **kwargs):
         context = super(UserDetailView, self).get_context_data(**kwargs)
         context.update({'page_name': self.object.name})
         return context
 
 
+class CreateUserView(CreateView):
+    template_name = 'user_create.html'
+    form_class = UserForm
+    model = User
+    extra_context = {'page_name': 'Add USER'}
 
-# def create_user_view(request):
-#     if request.method == 'POST':
-#         form = CreatePersonForm(request.POST)
-#         persons = User.objects.all()
-#         context = {
-#             'persons': persons
-#         }
-#         if form.is_valid():
-#             instance = form.save(commit=False)
-#             instance.save()
-#             return render(request, 'persons_list.html', context)
-#     else:
-#         form = CreatePersonForm()
-#     context = {'form': form}
-#     return render(request, 'create_person.html', context)
 
+class UpdateUserView(UpdateView):
+    template_name = 'user_update.html'
+    form_class = UserForm
+    model = User
+    extra_context = {'page_name': User}
+
+
+class DeleteUserView(DeleteView):
+    template_name = 'user_delete.html'
+    model = User
+    success_url = reverse_lazy('users')
+    extra_context = {'page_name': User}
