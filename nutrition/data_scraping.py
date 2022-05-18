3aed5fdbf23cd1bb9f66897b3d67d7c5b213d038
Katerina Kratka <katka.kratkaa@gmail,com>
2022-05-18 22:47:18 +0200

    Data_scraping.py updated

--- nutrition/data_scraping.py
+++ nutrition/data_scraping.py
@@ -1,21 +1,35 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-import scrapy
+import bs4 as bs
+import requests
+import re
 
 
-class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    start_urls = [
-        'https://quotes.toscrape.com/tag/humor/',
-    ]
+def searchMFP(search_term):
+    resp = requests.get('https://www.myfitnesspal.com/food/search?page=1&search=' + search_term)
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
 
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'author': quote.xpath('span/small/text()').get(),
-                'text': quote.css('span.text::text').get(),
-            }
+    food_item = soup.find('a', href=re.compile('^/food/calories/'))['href']
 
-        next_page = response.css('li.next a::attr("href")').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+    resp = requests.get('https://www.myfitnesspal.com' + food_item)
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+
+    nutrition = soup.find('div', 'NutritionalInfoContainer-3XIjH')
+
+    # print('https://www.myfitnesspal.com'+food_item)
+
+    nutritions = [x.getText(separator=u' ') for x in nutrition.find_all('div')]
+
+    base1 = nutritions[0].split('   g ')
+
+    del nutritions[0]
+
+    nutritions = base1 + nutritions
+
+    del nutritions[18]
+
+    nutritions = [x[:-4] if x.endswith('   g') else x for x in nutritions]
+    nutritions = [x[:-5] if x.endswith('   mg') else x for x in nutritions]
+    nutritions = [x[:-4] if x.endswith('   %') else x for x in nutritions]
+
+    nutritions = ['Calories ' + soup.find('h1').getText()] + nutritions
+
+    return dict(x.rsplit(' ', 1) for x in nutritions)
