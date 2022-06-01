99977b76c3cf1f4f9ba03f74caee9df1bbcb36f6
Káča <90765044+kachnazkoci@users.noreply.github.com>
2022-06-01 16:06:00 +0200

    Add files via upload
    food models edited (int->float)
    bulk_create_food.py edited + command in comment
    new food.csv saved nutrition/management/command/
    old food renamed
    toDO list created in static

---
+++ bulk_create_food.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from food.models import Food
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Adds food to the database from a CSV file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('food.csv', type=str)
+
+    def handle(self, *args, **options):
+        filename = options["food.csv"]
+        with open('nutrition/management/commands/food.csv', newline="") as f:
+            r = csv.reader(f)
+            next(r, None)  # skip the headers
+            food = []
+            for row in r:
+                food.append(
+                    Food(name=row[0], created='2022-06-01', kcal=row[1], carbs=row[2], protein=row[3], fats=row[4]))
+        print(len(Food.objects.bulk_create(food)))
+
+
+# python manage.py bulk_create_food food.csv command
---
+++ models.py
@@ -0,0 +1,96 @@
+from django.db import models
+from django.shortcuts import resolve_url
+from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
+
+#from food.forms import BlogForm
+from user.models import User
+
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=256)
+    created = models.DateField()
+    kcal = models.FloatField()
+    protein = models.FloatField()
+    fats = models.FloatField()
+    carbs = models.FloatField()
+    #note = models.CharField(max_length=256) # , required=False
+
+    def __str__(self):
+        return self.name
+
+    # class Meta:
+    #     abstract = True
+
+    def get_absolute_url(self):
+        return resolve_url('food_detail', pk=self.id)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=256)
+    created = models.DateField()
+    ingredients = models.ManyToManyField(Food, related_name='recipes', through='Amount')
+    weight = models.FloatField()
+    kcal = models.FloatField()
+    protein = models.FloatField()
+    fats = models.FloatField()
+    carbs = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return resolve_url('recipe_detail', pk=self.id)
+
+
+class Amount(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+
+class Blog(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=200, blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=250, blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    # def save(self, commit=True):
+    #     blog = super(BlogForm, self).save(commit=commit)
+    #     blog.food.add(*self.cleaned_data.get('food'))
+    #     return blog
+
+    # def __str__(self):
+    #     return self.name
+    class Meta:
+        verbose_name = 'Blog'
+        ordering = ["timestamp"]
+
+    def get_absolute_url(self):
+        return resolve_url('blog_detail', pk=self.pk)
+
+
+# class BlogPost(models.Model):
+#     title = models.CharField(max_length=255, blank=False, null=False)
+#     body = RichTextField(blank=False, null=False)
+
+# class BMICounter(models.Model):
+#     GENDER_MALE = 'male'
+#     GENDER_FEMALE = 'female'
+#
+#     GENDER_CHOICES = (
+#         (GENDER_MALE, 'male'),
+#         (GENDER_FEMALE, 'female')
+#     )
+#
+#     height = User.height
+#     weight = User.weight
+#     gender = User.gender
+#
+#
+#     def get_absolute_url(self):
+#         return resolve_url('counter_BMI', pk=self.id)
